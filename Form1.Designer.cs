c03a37037170de7b27fbfc59f8df8bae9dd427ac
Lenin Guaminga <leninguaminga@gmail.com>
2023-10-04 22:19:43 -0500

    incial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace HipptamoRloj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbHora = new System.Windows.Forms.Label();
+            this.lbFecha = new System.Windows.Forms.Label();
+            this.close = new System.Windows.Forms.Label();
+            this.HoraFecha = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbHora
+            // 
+            this.lbHora.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHora.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(196)))));
+            this.lbHora.Location = new System.Drawing.Point(249, 80);
+            this.lbHora.Name = "lbHora";
+            this.lbHora.Size = new System.Drawing.Size(189, 46);
+            this.lbHora.TabIndex = 1;
+            this.lbHora.Text = "label1";
+            this.lbHora.Click += new System.EventHandler(this.lbHora_Click);
+            // 
+            // lbFecha
+            // 
+            this.lbFecha.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFecha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(196)))));
+            this.lbFecha.Location = new System.Drawing.Point(124, 126);
+            this.lbFecha.Name = "lbFecha";
+            this.lbFecha.Size = new System.Drawing.Size(473, 31);
+            this.lbFecha.TabIndex = 2;
+            this.lbFecha.Text = "label1";
+            this.lbFecha.Click += new System.EventHandler(this.lbFecha_Click);
+            // 
+            // close
+            // 
+            this.close.AutoSize = true;
+            this.close.Font = new System.Drawing.Font("JetBrains Mono", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(125)))), ((int)(((byte)(145)))));
+            this.close.Location = new System.Drawing.Point(598, 9);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(23, 25);
+            this.close.TabIndex = 3;
+            this.close.Text = "x";
+            this.close.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // HoraFecha
+            // 
+            this.HoraFecha.Enabled = true;
+            this.HoraFecha.Tick += new System.EventHandler(this.HoraFecha_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(31)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(644, 257);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.lbFecha);
+            this.Controls.Add(this.lbHora);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbHora;
+        private System.Windows.Forms.Label lbFecha;
+        private System.Windows.Forms.Label close;
+        private System.Windows.Forms.Timer HoraFecha;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HipptamoRloj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void HoraFecha_Tick(object sender, EventArgs e)
+        {
+            lbHora.Text = DateTime.Now.ToString("HH:mm;ss");
+            lbFecha.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void lbHora_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbFecha_Click(object sender, EventArgs e)
+        {
+            lbFecha.TextAlign = ContentAlignment.MiddleCenter;
+            int padding = (lbFecha.Height - (int) lbFecha.Font.GetHeight())/ 2;
+            lbFecha.Padding = new Padding(0, padding, 0, padding);
+        }
+    }
+}
